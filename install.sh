b2c8f25ee7eb8af8a2709f159f867aa69a6e867e
Jonathan <jonathan.max.annett@gmail.com>
2021-05-28 10:51:50 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,16 +6,18 @@ if [ "$(whoami)" != "root" ] ; then
 fi
 
 apt-get update
-apt-get install tree htop inotify-tools squashfs-tools aufs-tools
+apt-get install tree htop inotify-tools squashfs-tools aufs-tools inetutils-traceroute
 
 if [ -e ./make-chroot-jail.sh ]; then
-  [ -e /usr/local/bin/make-chroot-jail ] && chmod 777 /usr/local/bin/make-chroot-jail && rm chmod 555 /usr/local/bin/make-chroot-jail
-  cp ./make-chroot-jail.sh /usr/local/bin/make-chroot-jail
-  chmod 555 /usr/local/bin/make-chroot-jail
-  echo "make-chroot-jail is now installed"
-  echo "usage : sudo make-chroot-jail username [ /path/to/app ]"
-  echo "   note - the username specifed should be that does not yet exist. "
-  echo "         (or has been previously used if you are updating)"
-  echo "        - if you don't specify /path/to/app,  /app will be assume"
-  echo "           this is the the source path that will be used to create the chroot file system"
+   [ -e /usr/local/bin/make-chroot-jail ] && chmod 777 /usr/local/bin/make-chroot-jail && rm chmod 555 /usr/local/bin/make-chroot-jail
+   cp ./make-chroot-jail.sh /usr/local/bin/make-chroot-jail
+   chmod 555 /usr/local/bin/make-chroot-jail
+   echo "make-chroot-jail is now installed"
+   echo "usage : sudo make-chroot-jail username [ /path/to/app ] [ramdiskMB]"
+   echo "   note - the username specifed should be that does not yet exist. "
+   echo "         (or has been previously used if you are updating)"
+   echo "        - if you don't specify /path/to/app,  /app will be assume"
+   echo "           this is the the source path that will be used to create the chroot file system"
+   echo "        - using 0 for ramdiskMB disables the ramdisk, and enables diagnostic mode "
+   
 fi
